b53df1b2b659bfa02322fca2c2ba856dc61dc744
Zack Umar <zackumar2@gmail.com>
2020-08-20 17:14:07 -0500

    Update webapi-types.ts

--- src/web-api/webapi-types.ts
+++ src/web-api/webapi-types.ts
@@ -140,3 +140,156 @@ interface CursorBasedPaging {
     cursors: Cursor
     total: number
 }
+
+interface PlayHistory {
+    track: Track
+    playedAt: Date //Timestamp
+    context: Context
+}
+
+interface Playlist {
+    collaborative: boolean
+    description: string | undefined
+    externalUrls: ExternalUrl
+    followers: Followers
+    href: string
+    id: string
+    images: APIImage[]
+    name: string
+    owner: User
+    public: boolean | undefined
+    snapshotId: string
+    tracks: Paging //[] inside paging object
+    type: string
+    uri: string
+}
+
+interface PlaylistTrack {
+    addedAt: Date //timestamp
+    addedBy: User
+    isLocal: boolean
+    track: Track | Episode
+}
+
+interface Recommendations {
+    seeds: RecommendationsSeed[]
+    tracks: Track[]
+}
+
+interface RecommendationsSeed {
+    afterFilteringSize: number
+    afterRelinkingSize: number
+    href: string
+    id: string
+    initialPoolSize: number
+    type: string
+}
+
+interface Restrictions {
+    reason: string
+}
+
+interface SavedTrack {
+    addedAt: Date //Timestamp
+    track: Track
+}
+
+interface SavedAlbum {
+    addedAt: Date //Timestamp
+    album: Album
+}
+
+interface SavedShow {
+    addedAt: Date //Timestamp
+    show: Show
+}
+
+interface Track {
+    album: Album
+    artist: Artist[]
+    availableMarkets: string[]
+    discNumber: number
+    durationMiliseconds: number
+    explicit: boolean
+    externalIds: ExternalId
+    externalUrls: ExternalUrl
+    href: string
+    id: string
+    isPlayable: boolean
+    linkedFrom: TrackLink
+    restrictions: Restrictions
+    name: string
+    popularity: number
+    previewUrl: string | undefined
+    trackNumber: number
+    type: string
+    uri: string
+    isLocal: boolean
+}
+
+interface TrackLink {
+    externalUrls: ExternalUrl
+    href: string
+    id: string
+    type: string
+    uri: string
+}
+
+interface Episode {
+    audioPreviewUrl: string
+    description: string
+    durationMiliseconds: number
+    explicit: boolean
+    externalUrls: ExternalUrl
+    href: string
+    id: string
+    images: APIImage[]
+    isExternallyHosted: boolean
+    isPlayable: boolean
+    languages: string[]
+    name: string
+    releaseDate: string
+    releaseDatePrecision: string
+    resumePoint: ResumePoint
+    show: Show
+    type: string
+    uri: string
+}
+
+interface ResumePoint {
+    fullyPlayed: boolean
+    resumePositionMiliseconds: number
+}
+
+interface Show {
+    availableMarkets: string[]
+    copyrights: Copyright[]
+    description: string
+    explicit: boolean
+    episodes: Paging //Episodes [] inside
+    externalUrls: ExternalUrl
+    href: string
+    id: string
+    images: APIImage[]
+    isExternallyHosted: boolean | undefined
+    languages: string[]
+    mediaType: string
+    name: string
+    publisher: string
+    type: string
+    uri: string
+}
+
+interface User {
+    country: string
+    displayName: string
+    email: string
+    externalUrls: ExternalUrl
+    followers: Followers
+    href: string
+    id: String
+    images: APIImage[]
+    product: string
+    type: string
+    uri: string
+}
